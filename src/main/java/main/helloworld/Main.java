493db916cd9d1468b19707cd72fd60764d219edd
Danijela.Savic <modestyblaisebg@gmail.com>
2020-02-06 19:58:57 +0100

    adds examples for differnt loops types

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -15,9 +15,7 @@ public class Main {
 
         System.out.println("Hello World");
         System.out.println("Danijela Savic");
-        
-       
-        
+
 //        int age = 18;
 //        
 //        if (age >= 18) {
@@ -57,38 +55,106 @@ public class Main {
 //        else { 
 //            System.out.println("Invalid");
 //        }
-       
-        int var;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Unesite redni broj dana (1 - 7): ");
-        var = input.nextInt();  
-        switch (var) {
-            case 1:
-                System.out.println("Ponedeljak");
-            break;
-            case 2:
-                System.out.println("Utorak");
-            break;
-            case 3:
-                System.out.println("Sreda");
-            break;
-            case 4:
-                System.out.println("Cetvrtak");
-            break;
-            case 5:
-                System.out.println("Petak");
-            break;
-            case 6:
-                System.out.println("Subota");
-            break;
-            case 7:
-                System.out.println("Nedelja");
-            break;
-            default:
-                System.out.println("Niste uneli dobar broj");
+//        int var;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Unesite redni broj dana (1 - 7): ");
+//        var = input.nextInt();  
+//        switch (var) {
+//            case 1:
+//                System.out.println("Ponedeljak");
+//            break;
+//            case 2:
+//                System.out.println("Utorak");
+//            break;
+//            case 3:
+//                System.out.println("Sreda");
+//            break;
+//            case 4:
+//                System.out.println("Cetvrtak");
+//            break;
+//            case 5:
+//                System.out.println("Petak");
+//            break;
+//            case 6:
+//                System.out.println("Subota");
+//            break;
+//            case 7:
+//                System.out.println("Nedelja");
+//            break;
+//            default:
+//                System.out.println("Niste uneli dobar broj");
+//        }
+        System.out.println("While loop:");
+
+        int x = 5;
+        while (x < 10) {
+            System.out.println(x);
+            x = x + 1;
         }
 
+        System.out.println("While loop2:");
+        x = 10;
+        while (x > 0) {
+            System.out.println(x);
+            x--;
+        }
+
+        System.out.println("Do While loop:");
+        x = 15;
+        do {
+            System.out.println(x);
+            x--;
+        } while (x > 15);
+
+        System.out.println("For loop:");
+
+        for (int i = 50; i < 60; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("For loop2:");
+
+        for (int j = 30; j > 19; j--) {
+            System.out.println(j);
+        }
+
+        for (int k = 0; k < 0; k++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int k = 0; k < 0; k++) {
+            System.out.print("*");
+        }
+        for (int k = 0; k < 0; k++) {
+            System.out.print("*");
+        }
+        for (int k = 0; k < 0; k++) {
+            System.out.print("*");
+        }
         
+        System.out.println("Kvadrat: ");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("Koordinate 1: ");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("(" + i + "," + j + ")");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("Koordinate 2: ");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("(" + i + "," + j + ")");
+            }
+            System.out.println();
+        }
     }
-    
+
 }
